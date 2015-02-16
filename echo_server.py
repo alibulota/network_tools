bf881db544766d28ccb2cf6b0c0aea67aeb1d4e4
Alicija Bulota <aabulota@smcm.edu>
2015-02-15 21:11:08 -0800

    added corrections from cris' comments

--- echo_server.py
+++ echo_server.py
@@ -17,18 +17,21 @@ def echo_server():
             msg_recieved = conn.recv(32)
             message += msg_recieved
             if len(msg_recieved) < 32:
-                print msg_recieved
                 break
         try:
             # This seems to be a mac-specific bug
             conn.shutdown(socket.SHUT_RD)
         except Exception, e:
             pass
+        print message
         conn.sendall(message)
         conn.shutdown(socket.SHUT_WR)
         conn.close()
+        if message == "quit":
+            break
     user_socket.close()
     return unicode(message, 'utf=8')
+
 
 if __name__ == '__main__':
     echo_server()
