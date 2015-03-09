d0b831a0f3b80cfcee84edfff82067f88422fdb1
Alicija Bulota <aabulota@smcm.edu>
2015-03-08 20:21:28 -0700

    add to response ok test

--- test_http.py
+++ test_http.py
@@ -1,12 +1,15 @@
 import pytest
 from echo_server import echo_server
 from echo_client import echo_client
-from http1 import response_ok, response_error, parse_request
+from http1 import http1, response_ok, response_error, parse_request
 
 
 def test_reponse_ok():
-    response = response_ok()
-    assert '200 OK' in response
+    response = '200 OK'
+    content_type = 'text/plain'
+    actual = http1.response_ok()
+    assert response in actual
+    assert content_type in actual
 
 
 def test_response_error():
